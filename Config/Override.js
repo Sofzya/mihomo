5acfa7ae33790dcf2da2e4b468106910675a4d54
Sofzya <124655584+Sofzya@users.noreply.github.com>
2026-04-27 20:02:05 +0800

    Add files via upload

--- Config/Override.js
+++ Config/Override.js
@@ -17,7 +17,7 @@ function main(config) {
     "exclude-filter": "流量|重置|到期|官网|网址|文档",
 };
   // 自定义分组通用配置
-  const commonProxies = ["DIRECT", "节点选择", "香港节点", "台湾节点", "日本节点", "狮城节点", "美国节点"];
+  const commonProxies = ["DIRECT", "节点选择", "香港节点", "台湾节点", "日本节点", "狮城节点", "美国节点", "家宽节点"];
   const proxyCount = config?.proxies?.length ?? 0;
   const proxyProviderCount = typeof config?.["proxy-providers"] === "object" ? Object.keys(config["proxy-providers"]).length : 0;
   if (proxyCount === 0 && proxyProviderCount === 0) {
@@ -25,7 +25,7 @@ function main(config) {
   }
 
   // 1. 基础配置覆盖
-  config["mixed-port"] = 7890; // 部分版本要求数字型而非字符串
+  config["mixed-port"] = 7893; // 部分版本要求数字型而非字符串
   config["tcp-concurrent"] = true;
   config["allow-lan"] = true;
   config["ipv6"] = true;
@@ -74,50 +74,53 @@ function main(config) {
 
   // 6. 策略组覆盖
   config["proxy-groups"] = [
-    { ...groupBaseOption, "name": "节点选择", "type": "select", "include-all": true, "proxies": ["香港节点", "台湾节点", "日本节点", "狮城节点", "美国节点"], "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Rocket.png" },
-    { ...groupBaseOption, "name": "国外网站", "type": "select", "proxies": commonProxies, "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Global.png" },
-    { ...groupBaseOption, "name": "Emby", "type": "select", "proxies": commonProxies, "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Emby.png" },
-    { ...groupBaseOption, "name": "云盘服务", "type": "select", "include-all": true, "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/iCloud.png" },
-    { ...groupBaseOption, "name": "电报消息", "type": "select", "proxies": commonProxies, "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Telegram.png" },
-    { ...groupBaseOption, "name": "推特消息", "type": "select", "proxies": commonProxies, "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Twitter.png" },
-    { ...groupBaseOption, "name": "AI", "type": "select", "proxies": commonProxies, "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/AI.png" },
+    { ...groupBaseOption, "name": "节点选择", "type": "select", "include-all": true, "proxies": ["香港节点", "台湾节点", "日本节点", "狮城节点", "美国节点"], "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/Rocket.png" },
+    { ...groupBaseOption, "name": "国外网站", "type": "select", "proxies": commonProxies, "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/Global.png" },
+    { ...groupBaseOption, "name": "Emby", "type": "select", "proxies": commonProxies, "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/Emby.png" },
+    { ...groupBaseOption, "name": "云盘服务", "type": "select", "include-all": true, "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/iCloud.png" },
+    { ...groupBaseOption, "name": "电报消息", "type": "select", "proxies": commonProxies, "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/Telegram.png" },
+    { ...groupBaseOption, "name": "推特消息", "type": "select", "proxies": commonProxies, "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/Twitter.png" },
+    { ...groupBaseOption, "name": "AI", "type": "select", "proxies": commonProxies, "icon": "https://github.com/Koolson/Qure/raw/raw/master/IconSet/Color/AI.png" },
     { ...groupBaseOption, "name": "E-Hentai", "type": "select", "proxies": commonProxies, "icon": "https://github.com/lige47/QuanX-icon-rule/raw/main/icon/04ProxySoft/exhentai.png" },
-    { ...groupBaseOption, "name": "游戏平台", "type": "select", "proxies": commonProxies, "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Game.png" },
-    { ...groupBaseOption, "name": "哔哩哔哩", "type": "select", "proxies": commonProxies, "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/bilibili.png" },
-    { ...groupBaseOption, "name": "巴哈姆特", "type": "select", "proxies": commonProxies, "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Bahamut.png" },
-    { ...groupBaseOption, "name": "谷歌服务", "type": "select", "proxies": commonProxies, "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Google_Search.png" },
-    { ...groupBaseOption, "name": "微软服务", "type": "select", "proxies": commonProxies, "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Microsoft.png" },
-    { ...groupBaseOption, "name": "数币平台", "type": "select", "proxies": commonProxies, "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Auto.png" },
-    { ...groupBaseOption, "name": "仅限日本", "type": "select", "proxies": commonProxies, "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/JP.png" },
-    { ...groupBaseOption, "name": "排除日本", "type": "select", "proxies": commonProxies, "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Reject.png" },
-    { ...groupBaseOption, "name": "兜底分流", "type": "select", "proxies": commonProxies, "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Final.png" },
+    { ...groupBaseOption, "name": "游戏平台", "type": "select", "proxies": commonProxies, "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/Game.png" },
+    { ...groupBaseOption, "name": "哔哩哔哩", "type": "select", "proxies": commonProxies, "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/bilibili.png" },
+    { ...groupBaseOption, "name": "巴哈姆特", "type": "select", "proxies": commonProxies, "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/Bahamut.png" },
+    { ...groupBaseOption, "name": "CloudFlare", "type": "select", "proxies": commonProxies, "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/Cloudflare.png" },
+    { ...groupBaseOption, "name": "谷歌服务", "type": "select", "proxies": commonProxies, "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/Google_Search.png" },
+    { ...groupBaseOption, "name": "微软服务", "type": "select", "proxies": commonProxies, "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/Microsoft.png" },
+    { ...groupBaseOption, "name": "数币平台", "type": "select", "proxies": commonProxies, "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/Auto.png" },
+    { ...groupBaseOption, "name": "仅限日本", "type": "select", "proxies": commonProxies, "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/JP.png" },
+    { ...groupBaseOption, "name": "排除日本", "type": "select", "proxies": commonProxies, "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/Reject.png" },
+    { ...groupBaseOption, "name": "兜底分流", "type": "select", "proxies": commonProxies, "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/Final.png" },
     // 地区分组
-    { ...groupBaseOption, "name": "香港节点", "type": "url-test", "tolerance": 0, "include-all": true, "filter": "(?i)🇭🇰|香港|(\\b(HK|Hong)\\b)", "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Hong_Kong.png" },
-    { ...groupBaseOption, "name": "台湾节点", "type": "url-test", "tolerance": 0, "include-all": true, "filter": "(?i)🇨🇳|🇹🇼|台湾|(\\b(TW|Tai|Taiwan)\\b)", "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Taiwan.png" },
-    { ...groupBaseOption, "name": "日本节点", "type": "url-test", "tolerance": 0, "include-all": true, "filter": "(?i)🇯🇵|日本|东京|(\\b(JP|Japan)\\b)", "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Japan.png" },
-    { ...groupBaseOption, "name": "狮城节点", "type": "url-test", "tolerance": 0, "include-all": true, "filter": "(?i)🇸🇬|新加坡|狮|(\\b(SG|Singapore)\\b)", "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Singapore.png" },
-    { ...groupBaseOption, "name": "美国节点", "type": "url-test", "tolerance": 0, "include-all": true, "filter": "(?i)🇺🇸|美国|洛杉矶|圣何塞|(\\b(US|United States)\\b)", "icon": "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/United_States.png" }
+    { ...groupBaseOption, "name": "香港节点", "type": "url-test", "tolerance": 0, "include-all": true, "filter": "(?i)🇭🇰|香港|(\\b(HK|Hong)\\b)", "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/Hong_Kong.png" },
+    { ...groupBaseOption, "name": "台湾节点", "type": "url-test", "tolerance": 0, "include-all": true, "filter": "(?i)🇨🇳|🇹🇼|台湾|(\\b(TW|Tai|Taiwan)\\b)", "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/Taiwan.png" },
+    { ...groupBaseOption, "name": "日本节点", "type": "url-test", "tolerance": 0, "include-all": true, "filter": "(?i)🇯🇵|日本|东京|(\\b(JP|Japan)\\b)", "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/Japan.png" },
+    { ...groupBaseOption, "name": "狮城节点", "type": "url-test", "tolerance": 0, "include-all": true, "filter": "(?i)🇸🇬|新加坡|狮|(\\b(SG|Singapore)\\b)", "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/Singapore.png" },
+    { ...groupBaseOption, "name": "美国节点", "type": "url-test", "tolerance": 0, "include-all": true, "filter": "(?i)🇺🇸|美国|洛杉矶|圣何塞|(\\b(US|United States)\\b)", "icon": "https://github.com/Koolson/Qure/raw/master/IconSet/Color/United_States.png" },
+    { ...groupBaseOption, "name": "家宽节点", "type": "url-test", "tolerance": 0, "include-all": true, "filter": "家宽", "icon": "https://github.com/Orz-3/mini/raw/master/Color/China.png" }
   ];
 
   // 7. 规则集配置
   config["rule-providers"] = {
-    "Direct": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Sofzya/Clash/raw/main/Rules/Direct.list" },
-    "CloudDrive": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Sofzya/Clash/raw/main/Rules/CloudDrive.list" },
+    "Direct": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Sofzya/mihomo/raw/main/Rules/Direct.list" },
+    "CloudDrive": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Sofzya/mihomo/raw/main/Rules/CloudDrive.list" },
     "MEGA": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/blackmatrix7/ios_rule_script/raw/master/rule/Clash/MEGA/MEGA.list" },
-    "OutJP": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Sofzya/Clash/raw/main/Rules/OutJP.list" },
-    "OnlyJP": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Sofzya/Clash/raw/main/Rules/OnlyJP.list" },
-    "Proxy": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Sofzya/Clash/raw/main/Rules/Proxy.list" },
-    "MoreEmby": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Sofzya/Clash/raw/main/Rules/MoreEmby.list" },
+    "OutJP": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Sofzya/mihomo/raw/main/Rules/OutJP.list" },
+    "OnlyJP": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Sofzya/mihomo/raw/main/Rules/OnlyJP.list" },
+    "Proxy": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Sofzya/mihomo/raw/main/Rules/Proxy.list" },
+    "MoreEmby": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Sofzya/mihomo/raw/main/Rules/MoreEmby.list" },
     "Emby": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Repcz/Tool/raw/X/mihomo/Rules/Emby.list" },
     "Telegram": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Repcz/Tool/raw/X/mihomo/Rules/Telegram.list" },
     "Twitter": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Repcz/Tool/raw/X/mihomo/Rules/Twitter.list" },
-    "MoreAI": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Sofzya/Clash/raw/main/Rules/MoreAI.list" },
+    "MoreAI": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Sofzya/mihomo/raw/main/Rules/MoreAI.list" },
     "AI": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Repcz/Tool/raw/X/mihomo/Rules/AI.list" },
     "E-Hentai": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/powerfullz/override-rules/raw/main/ruleset/EHentai.list" },
     "Steam": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Repcz/Tool/raw/X/mihomo/Rules/Steam.list" },
     "Epic": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Repcz/Tool/raw/X/mihomo/Rules/Epic.list" },
     "BiliBili": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Repcz/Tool/raw/X/mihomo/Rules/Bilibili.list" },
     "Bahamut": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Repcz/Tool/raw/X/mihomo/Rules/Bahamut.list" },
+    "CloudFlare": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Repcz/Tool/raw/X/mihomo/Rules/Cloudflare.list" },
     "YouTube": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Repcz/Tool/raw/X/mihomo/Rules/YouTube.list" },
     "Google": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Repcz/Tool/raw/X/mihomo/Rules/Google.list" },
     "Github": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Repcz/Tool/raw/X/mihomo/Rules/Github.list" },
@@ -125,7 +128,7 @@ function main(config) {
     "Microsoft": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Repcz/Tool/raw/X/mihomo/Rules/Microsoft.list" },
     "OKX": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/blackmatrix7/ios_rule_script/raw/master/rule/Clash/OKX/OKX.list" },
     "Binance": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/blackmatrix7/ios_rule_script/raw/master/rule/Clash/Binance/Binance.list" },
-    "Bybit": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Sofzya/Clash/raw/main/Rules/Bybit.list" },
+    "Bybit": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Sofzya/mihomo/raw/main/Rules/Bybit.list" },
     "Lan": { ...ruleProviderCommon, "behavior": "classical", "url": "https://github.com/Repcz/Tool/raw/X/mihomo/Rules/Lan.list" }
   };
 
@@ -148,6 +151,7 @@ function main(config) {
     "RULE-SET,Epic,游戏平台",
     "RULE-SET,BiliBili,哔哩哔哩",
     "RULE-SET,Bahamut,巴哈姆特",
+    "RULE-SET,CloudFlare,CloudFlare",
     "RULE-SET,YouTube,谷歌服务",
     "RULE-SET,Google,谷歌服务",
     "RULE-SET,Github,微软服务",
